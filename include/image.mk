6333a4db0bcd748b10b8b178e15fc5964aa313d9
bryan newbold <bnewbold@robocracy.org>
2015-10-14 20:46:25 -0700

    build system: have tar use top-level $TIMESTAMP for --mtime
    The --mtime argument to 'tar' sets the modification time for all files within
    the archive, which determines the timestamp files will get when they are
    extracted. In this case, rootfs and other tarballs will get mtimes which
    correspond to the last commit timestamp of the build system, as reported by
    git/subversion.
    
    This is a step towards reproducible image builds.

--- include/image.mk
+++ include/image.mk
@@ -142,7 +142,7 @@ endef
 # $(3) extra CPP flags
 # $(4) extra DTC flags
 define Image/BuildDTB
-	$(TARGET_CROSS)cpp -nostdinc -x assembler-with-cpp \
+	$(CPP) -nostdinc -x assembler-with-cpp \
 		-I$(DTS_DIR) \
 		-I$(DTS_DIR)/include \
 		-undef -D__DTS__ $(3) \
@@ -255,7 +255,7 @@ define Image/mkfs/cpiogz
 endef
 
 define Image/mkfs/targz
-	$(TAR) -czpf $(BIN_DIR)/$(IMG_PREFIX)$(if $(PROFILE_SANITIZED),-$(PROFILE_SANITIZED))-rootfs.tar.gz --numeric-owner --owner=0 --group=0 --sort=name -C $(TARGET_DIR)/ .
+	$(TAR) -czpf $(BIN_DIR)/$(IMG_PREFIX)$(if $(PROFILE_SANITIZED),-$(PROFILE_SANITIZED))-rootfs.tar.gz --numeric-owner --owner=0 --group=0 --sort=name -C $(TARGET_DIR)/ . --mtime="$(TIMESTAMP)"
 endef
 
 E2SIZE=$(shell echo $$(($(CONFIG_TARGET_ROOTFS_PARTSIZE)*1024*1024)))
